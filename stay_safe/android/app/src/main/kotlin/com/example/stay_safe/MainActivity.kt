4c8c2d17fb62044f475723913b422cb0c47d4420
rooknpown <rooknpown@kaist.ac.kr>
2020-11-22 17:48:17 +0900

    search application + home screen

--- stay_safe/android/app/src/main/kotlin/com/example/stay_safe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stay_safe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- stay_safe/lib/main.dart
+++ stay_safe/lib/main.dart
@@ -1,117 +1,245 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_place_picker/google_maps_place_picker.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// Your api key storage.
+// import 'keys.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // Light Theme
+  final ThemeData lightTheme = ThemeData.light().copyWith(
+    // Background color of the FloatingCard
+    cardColor: Colors.white,
+    buttonTheme: ButtonThemeData(
+      // Select here's button color
+      buttonColor: Colors.black,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
+
+  // Dark Theme
+  final ThemeData darkTheme = ThemeData.dark().copyWith(
+    // Background color of the FloatingCard
+    cardColor: Colors.grey,
+    buttonTheme: ButtonThemeData(
+      // Select here's button color
+      buttonColor: Colors.yellow,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Google Map Place Picker Demo',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.light,
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  static final kInitialPosition = LatLng(-33.8567844, 151.213108);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  PickResult selectedPlace;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Stay Safe"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        bottomNavigationBar: BottomAppBar(
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Scaffold(
+                            appBar: AppBar(
+                              title: Text('Search Places. Find Safety'),
+                              automaticallyImplyLeading: false,
+                            ),
+                            body: PlacePicker(
+                              apiKey: "AIzaSyDqOOHRnNiYaCweRNtiXVQswGAb1Pz88Yc",
+                              initialPosition: HomePage.kInitialPosition,
+                              useCurrentLocation: true,
+                              selectInitialPosition: true,
+
+                              //usePlaceDetailSearch: true,
+                              onPlacePicked: (result) {
+                                selectedPlace = result;
+                                Navigator.of(context).pop();
+                                setState(() {});
+                              },
+                              //forceSearchOnZoomChanged: true,
+                              //automaticallyImplyAppBarLeading: false,
+                              //autocompleteLanguage: "ko",
+                              //region: 'au',
+                              //selectInitialPosition: true,
+                              selectedPlaceWidgetBuilder: (_, selectedPlace,
+                                  state, isSearchBarFocused) {
+                                print(
+                                    "state: $state, isSearchBarFocused: $isSearchBarFocused");
+                                return isSearchBarFocused
+                                    ? Container()
+                                    : FloatingCard(
+                                        bottomPosition:
+                                            0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
+                                        leftPosition: 0.0,
+                                        rightPosition: 0.0,
+                                        width: 500,
+                                        borderRadius:
+                                            BorderRadius.circular(12.0),
+                                        child: state == SearchingState.Searching
+                                            ? Center(
+                                                child:
+                                                    CircularProgressIndicator())
+                                            : RaisedButton(
+                                                child: Text("Pick Here"),
+                                                onPressed: () {
+                                                  // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
+                                                  //            this will override default 'Select here' Button.
+                                                  print(
+                                                      "do something with [selectedPlace] data");
+                                                  print(selectedPlace.placeId);
+                                                  print(selectedPlace.rating);
+                                                  Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          SearchResult(
+                                                              selectedPlace:
+                                                                  selectedPlace),
+                                                    ),
+                                                  );
+                                                  //Navigator.of(context).pop();
+                                                },
+                                              ),
+                                      );
+                              },
+                              // pinBuilder: (context, state) {
+                              //   if (state == PinState.Idle) {
+                              //     return Icon(Icons.favorite_border);
+                              //   } else {
+                              //     return Icon(Icons.favorite);
+                              //   }
+                              // },
+                            ));
+                      },
+                    ),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.account_box),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class SearchResult extends StatelessWidget {
+  final PickResult selectedPlace;
+
+  String buildPhotoURL(String photoReference) {
+    return "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference=${photoReference}&key=AIzaSyDqOOHRnNiYaCweRNtiXVQswGAb1Pz88Yc";
+  }
+
+  SearchResult({@required this.selectedPlace});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(selectedPlace.name),
+        ),
+        body: ListView(
+          children: [
+            Padding(
+                padding: EdgeInsets.only(right: 1.0),
+                child: SizedBox(
+                  height: 400,
+                  child: Image.network(
+                      buildPhotoURL(selectedPlace.photos[0].photoReference),
+                      height: 400,
+                      fit: BoxFit.fill),
+                )),
+            Text('Safety Level', style: TextStyle(height: 1, fontSize: 25)),
+            Center(
+                child: RatingBar.builder(
+              initialRating: 3,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+              itemSize: 65.0,
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (rating) {
+                print(rating);
+              },
+            )),
+            Text('Overall Experience',
+                style: TextStyle(height: 1, fontSize: 25)),
+            Center(
+                child: RatingBar.builder(
+              initialRating: selectedPlace.rating,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+              itemSize: 65.0,
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Colors.lightBlue,
+              ),
+              onRatingUpdate: (rating) {
+                print(rating);
+              },
+            )),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('SEE ALL REVIEWS'),
+              ),
+            )
+          ],
+        ));
   }
 }
